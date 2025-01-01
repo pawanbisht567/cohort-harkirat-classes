83f9228678ec3ec88ab9ececaeae75387727e0d1
pbisht17 <pawanbisht94.pb@gmail.com>
2025-01-01 23:18:01 +0530

    Next JS week 14.2

---
+++ Week 14, NextJS/week-14.2/app/api/user/route.ts
@@ -0,0 +1,20 @@
+export function GET() {
+    return Response.json({
+        name: "Pawan",
+        email: "Pawan@gmail.com"
+    })
+}
+
+export function POST() {
+    return Response.json({
+        name: "Pawan",
+        email: "Pawan@gmail.com"
+    })
+}
+
+export function PUT() {
+    return Response.json({
+        name: "Pawan",
+        email: "Pawan@gmail.com"
+    })
+}
